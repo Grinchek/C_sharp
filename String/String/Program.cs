2dc719711796ec8adb16b842ced89541a040e3e9
DEV <dev@step.rivne>
2023-12-15 11:35:40 +0200

    commit

---
+++ String/String/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace my_prog
+{
+    class My_class
+    {
+        static void Main(string[] args)
+        {
+            //pt1
+            //string st1 = "First string"; 
+            //string st2 = "inserted ";
+            //st1 = st1.Insert(6, st2);
+            //Console.WriteLine(st1);
+            //pt2
+            //bool IsPalindrom(string value)
+            //{
+            //    for (int i = 0; i < value.Length/2; i++)
+            //    {
+            //        if (value[i] != value[value.Length - i - 1])
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    return true; 
+            //}
+            //Console.WriteLine(IsPalindrom("otto"));
+            //p3
+
+        }
+    }
+
+}
